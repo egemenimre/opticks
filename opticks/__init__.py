ac715c09ea38536b553effccf9699f47dd86ecd4
egeimre-constellr <126004467+egeimre-constellr@users.noreply.github.com>
2024-02-13 14:10:59 +0100

    init packages

---
+++ opticks/__init__.py
@@ -0,0 +1,12 @@
+# opticks: Sizing Tool for Optical Systems
+#
+# Copyright (C) 2024 Egemen Imre
+#
+# Licensed under GNU GPL v3.0. See LICENSE.md for more info.
+__version__ = "0.0.1"
+
+from pint import UnitRegistry
+
+# Init units
+u = UnitRegistry()
+Q_ = u.Quantity
---
+++ opticks/schemas/__init__.py
@@ -0,0 +1,5 @@
+# opticks: Sizing Tool for Optical Systems
+#
+# Copyright (C) 2024 Egemen Imre
+#
+# Licensed under GNU GPL v3.0. See LICENSE.md for more info.
