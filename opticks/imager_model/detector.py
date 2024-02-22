a4e8e2ed14bb7fdaa50e1c7f692d24857f74ce12
egeimre-constellr <126004467+egeimre-constellr@users.noreply.github.com>
2024-02-22 16:25:11 +0100

    added modelling functions

--- opticks/imager_model/detector.py
+++ opticks/imager_model/detector.py
@@ -57,3 +57,101 @@ class Detector(ImagerComponent):
         return "DetectorParams"
 
     # ---------- begin modelling functions ----------
+
+    def pix_pitch(self, with_binning: bool = True) -> Quantity:
+        """
+        Returns pixel pitch parameter with or without binning.
+
+        Parameters
+        ----------
+        with_binning : bool
+            Return the value with binning or not
+
+        Returns
+        -------
+        Quantity
+            Pixel pitch with or without binning
+        """
+        binning = self.params.binning if with_binning else 1
+
+        return self.params.pixel_pitch * binning
+
+    def nyquist_freq(self, with_binning: bool = True) -> Quantity:
+        """
+        Returns Nyquist Frequency / Limit parameter with or without binning.
+
+        Nyquist frequency is defined as: $\frac{1}{2 \times \text{pix pitch}}$,
+        which translates to one line pair per two pixels (e.g., one pixel white,
+        next one black). This is the absolute maximum limiting resolution of the sensor.
+
+        Parameters
+        ----------
+        with_binning : bool
+            Return the value with binning or not
+
+        Returns
+        -------
+        Quantity
+            Nyquist limit in lp/mm
+        """
+
+        u.define("lp = 1 * dimensionless = lp")
+
+        return 1 * u.lp / (2 * self.pix_pitch(with_binning)).to(u.mm)
+
+    def pixel_count(self, with_binning: bool = True, used: bool = True) -> Quantity:
+        """
+        Computes the total number of pixels in the detector with/without binning
+        or total pixels/used pixels.
+
+        Parameters
+        ----------
+        with_binning : bool
+            Return the value with binning or not
+        used : bool
+            Return the value for used or total number of pixels
+        Returns
+        -------
+        Quantity
+            Total number of pixels for the requested configuration
+        """
+
+        binning = self.params.binning if with_binning else 1
+
+        if used:
+            # number of used pixels in the frame
+            return (
+                self.params.horizontal_pixels_used
+                / binning
+                * self.params.vertical_pixels_used
+                / binning
+                * u.pixel
+            ).to("Mpixel")
+        else:
+            # total number of pixels in the frame
+            return (
+                self.params.horizontal_pixels
+                / binning
+                * self.params.vertical_pixels
+                / binning
+                * u.pixel
+            ).to("Mpixel")
+
+    def pix_area(self, with_binning: bool = True) -> Quantity:
+        """
+        Pixel physical area.
+
+        Computed as $text{pix pitch} \times \text{pix pitch}$
+
+        Parameters
+        ----------
+        with_binning : bool
+            Return the value with binning or not
+
+        Returns
+        -------
+        Quantity
+            Pixel area with or without binning
+        """
+
+        return self.pix_pitch(with_binning) ** 2
