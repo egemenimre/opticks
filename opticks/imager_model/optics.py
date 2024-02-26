786820e9d5468258d529bfd8bd7851202c03350c
egeimre-constellr <126004467+egeimre-constellr@users.noreply.github.com>
2024-02-26 23:31:11 +0100

    added docstrings

--- opticks/imager_model/optics.py
+++ opticks/imager_model/optics.py
@@ -38,6 +38,11 @@ class Optics(ImagerComponent):
 
     @property
     def f_number(self) -> float:
+        """
+        F-number.
+
+        Computed as: $\frac{\text{focal length}}{\text{aperture diameter}}$
+        """
         return (
             (self.params.focal_length / self.params.aperture_diameter)
             .to_reduced_units()
@@ -46,17 +51,33 @@ class Optics(ImagerComponent):
 
     @property
     def full_optical_fov(self) -> Quantity:
+        """
+        Full optical Field of View.
+
+        Actual FoV depends on the detector size, but cannot be wider than this value.
+
+        Computed as: $2 \times \arctan\left( \frac{0.5 \times \text{image diameter on focal plane}}{\text{focal length}} \right)$
+        """
         return 2 * np.arctan(
             (self.params.image_diam_on_focal_plane / 2.0) / self.params.focal_length
         ).to(u.deg)
 
     @property
     def aperture_area(self) -> Quantity:
+        """
+        Aperture area.
+
+        Computed as: $\pi \times \left( \frac{ \text{aperture diameter}}{2} \right)^2$
+        """
         return np.pi * (self.params.aperture_diameter / 2.0) ** 2
 
     @property
     def aperture_solid_angle(self) -> Quantity:
-        # solid angle = 2pi h/r
+        """
+        Aperture solid angle in steradians.
+
+        Computed as: $\frac{\pi}{4} \frac{\text{aperture diameter}^2}{\text{focal length}^2}$
+        """
         return (
             np.pi
             / (self.params.focal_length / (self.params.aperture_diameter / 2.0)) ** 2
@@ -65,6 +86,23 @@ class Optics(ImagerComponent):
 
     @u.check(None, "[length]")
     def spatial_cutoff_freq(self, ref_wavelength: Quantity) -> Quantity:
+        """
+        Spatial cutoff frequency, assumes perfect incoherent optics.
+
+        Determines the theoretical limit of the optical resolution, or the smallest object resolvable by the optical system.
+
+        Computed as: $ 1 \over {\lambda F_\#} $ in line pairs per mm
+
+        Parameters
+        ----------
+        ref_wavelength : Quantity
+            Reference wavelength
+
+        Returns
+        -------
+        Quantity
+            Spatial cutoff frequency (in lp/mm)
+        """
         u.define("lp = 1 * dimensionless = lp")
         # perfect incoherent optics
         return (1.0 * u.lp) / (ref_wavelength * self.f_number).to(u.mm)
