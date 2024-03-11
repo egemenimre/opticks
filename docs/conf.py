9b51844c9ed3b16ada1c3f23399bf58d9a77e103
egemenimre <s.egemen.imre@gmail.com>
2024-03-11 22:26:51 +0100

    commented out githubpages extension

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ extensions = [
     "sphinx_copybutton",
     "notfound.extension",
     "hoverxref.extension",
-    "sphinx.ext.githubpages",
+    # "sphinx.ext.githubpages",
     "sphinx.ext.doctest",  # Doctest
     "myst_parser",  # MyST parser
     "nbsphinx",  # Jupyter notebook support
@@ -96,6 +96,7 @@ html_theme_path = [
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
 
 # -- Options for Intersphinx -------------------------------------------------
 
