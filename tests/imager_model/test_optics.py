7d9e53b9bf57f9548ecc4e62cdcb5322f34551b7
egemenimre <s.egemen.imre@gmail.com>
2024-10-04 08:34:59 +0200

    added psf test

--- tests/imager_model/test_optics.py
+++ tests/imager_model/test_optics.py
@@ -10,9 +10,11 @@ import numpy as np
 import pytest
 from prysm.coordinates import cart_to_polar, make_xy_grid
 from prysm.geometry import circle
+from prysm import coordinates, geometry, propagation, polynomials, _richdata
 
 from opticks import process_paths, u
 from opticks.imager_model.optics import ApertureFactory, Optics
+from opticks.utils.prysm_utils import OptPathDiff
 from opticks.utils.testing_utils import assert_allclose
 
 
@@ -94,3 +96,98 @@ class TestOptics:
 
         # verification
         np.testing.assert_array_equal(aperture, aperture_prysm, strict=True)
+
+    def test_psf(self):
+
+        # prysm set up
+        ap_samples = 256
+        res = 512  # psf_samples
+        fno = 4  # F number
+        efl = 150  # effective focal length
+        epd = efl / fno  # aperture diameter
+        r_aper = epd / 2  # aperture radius
+        wvl0 = 0.550  # wavelength in microns
+
+        # sample size in microns
+        res_el = wvl0 * fno * 1.22 / 4  # 4 pixels per airy radius
+
+        xi, eta = coordinates.make_xy_grid(ap_samples, diameter=epd)
+        r, t = coordinates.cart_to_polar(xi, eta)
+        dx = xi[0, 1] - xi[0, 0]
+
+        r_aber = r / r_aper
+
+        coef = wvl0 * 1e3 * 15  # 15 waves of defocus
+        phs = polynomials.hopkins(0, 2, 0, r_aber, t, 1) * coef
+
+        amp = geometry.circle(r_aper, r)
+
+        # monochromatic psf
+        wf = propagation.Wavefront.from_amp_and_phase(amp, phs, wvl0, dx)
+        psf_mono_prysm = wf.focus_fixed_sampling(efl, res_el, res).intensity
+
+        # polychromatic psf
+        halfbw = 0.2
+        wvls = np.linspace(
+            wvl0 * (1 - halfbw), wvl0 * (1 + halfbw), 11
+        )  # 11 discrete wavelengths
+        spectral_weights = np.ones_like(wvls)
+
+        components = []
+        for wvl in wvls:
+            wf = propagation.Wavefront.from_amp_and_phase(amp, phs, wvl, dx)
+            focused = wf.focus_fixed_sampling(
+                efl, res_el, res
+            )  # 512 samples in the output domain
+            components.append(
+                focused.intensity.data
+            )  # sum of intensities, wvls are incoherent to each other
+
+        # psf is just an array
+        psf_poly_prysm = polynomials.sum_of_2d_modes(components, spectral_weights)
+        # until we enrich it
+        psf_poly_prysm = _richdata.RichData(psf_poly_prysm, res_el, wvl0)
+
+        # ---------
+        # opticks set up
+        yaml_text = """
+        name: Prysm Test data
+        focal_length: 150 mm
+        aperture_diameter: 37.5 mm
+        image_diam_on_focal_plane: 10 mm # unknown"""
+
+        optics = Optics.from_yaml_text(yaml_text)
+
+        aperture, grid = ApertureFactory.circle_aperture(
+            optics.params.aperture_diameter, ap_samples, with_units=True
+        )
+        optics.set_aperture_model(aperture)
+
+        # compute opd
+        opd = OptPathDiff(phs * u.nm)
+
+        # generate and add the Wavefront or Pupil function (mono)
+        optics.add_pupil_func(wvl0 * u.um, opd)
+
+        # compute monochromatic PSF
+        psf_mono = optics.psf(
+            wvl0 * u.um, res_el * u.um, res, spectral_weights=None, with_units=True
+        )
+
+        # polychromatic psf
+
+        # reset pupil functs
+        optics.pupils = []
+
+        # reuse the same opd
+        for wvl in wvls:
+            optics.add_pupil_func(wvl * u.um, opd)
+
+        # compute polychromatic PSF
+        psf_poly = optics.psf(
+            wvl0 * u.um, res_el * u.um, res, spectral_weights=None, with_units=True
+        )
+
+        # verification
+        np.testing.assert_allclose(psf_mono.data, psf_mono_prysm.data, rtol=1e-10)
+        np.testing.assert_allclose(psf_poly.data, psf_poly_prysm.data, rtol=1e-10)
