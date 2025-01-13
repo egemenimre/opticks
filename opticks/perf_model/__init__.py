57c18dfe582e6a5df94251472fd29e8ef4c0d9f5
egemenimre <s.egemen.imre@gmail.com>
2025-01-13 17:18:23 +0100

    renamed perf_model to contrast_model

--- opticks/perf_model/__init__.py
+++
@@ -1,5 +0,0 @@
-# opticks: Sizing Tool for Optical Systems
-#
-# Copyright (C) Egemen Imre
-#
-# Licensed under GNU GPL v3.0. See LICENSE.md for more info.
--- tests/perf_model/__init__.py
+++
@@ -1,5 +0,0 @@
-# opticks: Sizing Tool for Optical Systems
-#
-# Copyright (C) 2024 Egemen Imre
-#
-# Licensed under GNU GPL v3.0. See LICENSE.md for more info.
